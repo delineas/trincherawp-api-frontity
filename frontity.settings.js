41935c76137f8864ab3c8baeb3b7172441eead20
Daniel Primo <delineas@gmail.com>
2020-02-17 16:06:27 +0100

    First commit

---
+++ frontity.settings.js
@@ -0,0 +1,42 @@
+const settings = {
+  name: "my-app",
+  state: {
+    frontity: {
+      url: "https://test.frontity.io",
+      title: "Test Frontity Blog",
+      description: "WordPress installation for Frontity development"
+    }
+  },
+  packages: [
+    {
+      name: "@frontity/mars-theme",
+      state: {
+        theme: {
+          menu: [
+            ["Home", "/"],
+            ["Nature", "/category/nature/"],
+            ["Travel", "/category/travel/"],
+            ["Aspernatur", "/aspernatur-nihil-ea-nulla/"],
+            ["About Us", "/about-us/"]
+          ],
+          featured: {
+            showOnList: false,
+            showOnPost: false
+          }
+        }
+      }
+    },
+    {
+      name: "@frontity/wp-source",
+      state: {
+        source: {
+          api: "https://wordpress-232126-1164206.cloudwaysapps.com/wp-json"
+        }
+      }
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react"
+  ]
+};
+
+export default settings;
